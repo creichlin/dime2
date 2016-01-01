3a2e4006264679758aa4fbc600f09f865809408f
Christian Reichlin <chrigi@kerbtier.ch>
2016-01-01 20:05:16 +0100

    in views objects can be fetched by slug

--- core/src/main/java/ch/kerbtier/dime2/site/renderer/RenderHelper.java
+++ core/src/main/java/ch/kerbtier/dime2/site/renderer/RenderHelper.java
@@ -2,6 +2,7 @@ package ch.kerbtier.dime2.site.renderer;
 
 import java.io.IOException;
 
+import ch.kerbtier.dime2.Models;
 import ch.kerbtier.dime2.Modules;
 import ch.kerbtier.dime2.modules.Page;
 import ch.kerbtier.esdi.Inject;
@@ -14,24 +15,35 @@ import com.github.jknack.handlebars.Helper;
 import com.github.jknack.handlebars.Options;
 
 @Inject
-public class RenderHelper implements Helper<HObject> {
+public class RenderHelper implements Helper<Object> {
   
   @InjectSingleton
   private Modules modules;
 
+  @InjectSingleton
+  private Models models;
+
   @Override
-  public CharSequence apply(HObject model, Options options) {
+  public CharSequence apply(Object model, Options options) {
     
     Process p = Process.get();
+    
+    if(model instanceof String) {
+      model = models.get().get("#" + model);
+    }
     
     if(model == null) {
       throw new RuntimeException("model must not be null");
     }
     
+    if(!(model instanceof HObject)) {
+      throw new RuntimeException("model must be object or slug");
+    }
+    
     String style = options.param(0) == null ? "default" : (String)options.param(0);
     String ext = p.getExtension();
     
-    Page page = modules.getPage(model.getName() + "." + style + "." + ext);
+    Page page = modules.getPage(((HObject)model).getName() + "." + style + "." + ext);
     try {
       Context context = Context.newBuilder(model).resolver(HNodeResolver.INSTANCE).build();
       return new Handlebars.SafeString(p.compile(page).apply(context));
